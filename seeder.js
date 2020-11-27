c3d69b84ea9cfea40674cdaeb06eab69feda6036
JSDale <57218102+JSDale@users.noreply.github.com>
2020-11-27 10:34:07 +0000

    created test data in JSON file

--- seeder.js
+++ seeder.js
@@ -2,5 +2,23 @@ const { MongoClient } = require("mongodb");
 const fs = require("fs").promises;
 const path = require("path");
 const loading = require("loading-cli");
-const uri = "mongodb://localhost:27017/wine";
+const { parse } = require("path");
+const uri = "mongodb://localhost:27017/YouthGroup";
 const client = new MongoClient(uri);
+
+async function main() {
+    await client.connect();
+    const db = client.db();
+    const results = await db.collection("youths").find({}).count();
+
+    //if there is no data in db, import it from JSON file
+    if(!results)
+    {
+        const data = await fs.readFile(path.join(__dirname, "youthGroup.json"), "utf-8");
+        await db.collection("youths").insertMany(JSON.parse(data));
+    }
+
+    
+}
+
+main();
